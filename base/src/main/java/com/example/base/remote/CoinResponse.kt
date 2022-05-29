174106c4d2dea5b2a064c72a8f53c9e537e4c966
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-05-29 18:51:04 +0100

    initial commit

---
+++ base/src/main/java/com/example/base/remote/CoinResponse.kt
@@ -0,0 +1,3 @@
+package com.example.base.remote
+
+class CoinResponse : ArrayList<CoinResponseItem>()
---
+++ base/src/main/java/com/example/base/remote/CoinResponseItem.kt
@@ -0,0 +1,11 @@
+package com.example.base.remote
+
+data class CoinResponseItem(
+    val id: String,
+    val is_active: Boolean,
+    val is_new: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
